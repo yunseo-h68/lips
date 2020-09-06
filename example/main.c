e990c54a591ade8ab27d0c7b1a3fbc1b4e8f3aec
Yunseo Hwang <yunseo.h68@gmail.com>
2020-09-06 10:43:25 +0900

    Update example

--- example/main.c
+++ example/main.c
@@ -1,10 +1,57 @@
 #include <stdio.h>
 #include "../include/lips.h"
 
-struct lips_args* init(int argc, char* argv[])
+void add_options(struct lips_args* args);
+void add_subcommands(struct lips_args* args);
+void print_help();
+void print_other(struct lips_args* args);
+void print_option_value(struct lips_args* args, const char* name);
+
+int main(int argc, char* argv[]) 
 {
+	int i = 0;
 	struct lips_args* args = new_lips_args();
-	
+	add_options(args);
+	add_subcommands(args);
+	lips_parse_args(args, argc, argv);
+
+	if (argc == 1) {
+		print_help();
+		return 0;
+	}
+
+	// Subcommands
+	if (lips_is_exist_subcommand(args,"hello")) {
+		printf("hello\n");
+		delete_lips_args(args);
+		return 0;
+	} else if (lips_is_exist_subcommand(args, "world")) {
+		printf("world\n");
+		delete_lips_args(args);
+		return 0;
+	} else if (lips_is_exist_subcommand(args, "test")) {
+		printf("#TEST\n");
+		print_other(args);
+		printf("Subcommand : %s\n", args->subcommand);
+		delete_lips_args(args);
+		return 0;
+	}
+
+	// Options
+	if (lips_is_exist_option(args, "h") || lips_is_exist_option_long(args, "help")) {
+		print_help();
+	} else if (lips_is_exist_option(args, "v") || lips_is_exist_option_long(args, "version")) {
+		printf("version 1.0.0\n");
+	} else if (lips_is_exist_option_long(args, "echo")) {
+		print_option_value(args, "echo");
+	}
+
+	delete_lips_args(args);
+	return 0;
+}
+
+void add_options(struct lips_args* args)
+{
 	// Add -h,--help Option
 	lips_add_option(args, "h");
 	lips_add_option_long(args, "help");
@@ -15,7 +62,10 @@ struct lips_args* init(int argc, char* argv[])
 	
 	// Add --echo Option
 	lips_add_option_long(args, "echo");
-	
+}
+
+void add_subcommands(struct lips_args* args)
+{
 	// Add hello Subcommand
 	lips_add_subcommand(args, "hello");
 	
@@ -24,8 +74,6 @@ struct lips_args* init(int argc, char* argv[])
 
 	// Add test Subcommand
 	lips_add_subcommand(args, "test");
-
-	return args;
 }
 
 void print_help() 
@@ -43,78 +91,28 @@ void print_help()
 	printf("\ttest  : print subcommand and other arguments\n\n");
 }
 
-void print_version() 
+void print_other(struct lips_args* args)
 {
-	printf("version 0.1.6\n");
+	int i = 0;
+	printf("-other args-\n");
+	printf("--use lips_get_other()\n");
+	char** other = lips_get_other(args);
+	for (i = 0; i < args->count_other_args; i++) {
+		printf("%s\n", other[i]);
+	}
+	printf("--or access other_args field\n");
+	for (i = 0; i < args->count_other_args; i++) {
+		printf("%s\n", args->other_args[i]);
+	}
+	printf("\n");
 }
 
-void print_hello() 
+void print_option_value(struct lips_args* args, const char* name) 
 {
-	printf("Hello\n");
-}
-
-void print_world() 
-{
-	printf("World\n");
-}
-
-void option_echo(struct lips_args* args) 
-{
-	char* str = lips_get_option_long_value(args, "echo");
+	char* str = lips_get_option_long_value(args, name);
 	if (str == NULL) {
 		print_help();
 		return;
 	}
 	printf("%s\n", str);
-}
-
-void print_other_args(struct lips_args* args)
-{
-	int i = 0; 
-	printf("\nTEST - Print other arguments(not subcommand, not options)\n");
-	for (i = 0; i < args->count_other_args; i++) {
-		printf("- %s\n", args->other_args[i]);
-	}
-}
-
-void print_subcommand(struct lips_args* args)
-{
-	printf("\nSubcommand : %s\n", args->subcommand);
-}
-
-int main(int argc, char* argv[]) 
-{
-	int i = 0;
-	struct lips_args* args = init(argc, argv);
-	lips_parse_args(args, argc, argv);
-
-	if (argc == 1) {
-		print_help();
-		return 0;
-	}
-
-	// Subcommands
-	if (lips_is_exist_subcommand(args,"hello")) {
-		print_hello();
-		return 0;
-	} else if (lips_is_exist_subcommand(args, "world")) {
-		print_world();
-		return 0;
-	} else if (lips_is_exist_subcommand(args, "test")) {
-		print_other_args(args);
-		print_subcommand(args);
-		return 0;
-	}
-
-	// Options
-	if (lips_is_exist_option(args, "h") || lips_is_exist_option_long(args, "help")) {
-		print_help();
-	} else if (lips_is_exist_option(args, "v") || lips_is_exist_option_long(args, "version")) {
-		print_version();
-	} else if (lips_is_exist_option_long(args, "echo")) {
-		option_echo(args);
-	}
-
-	delete_lips_args(args);
-	return 0;
 }
